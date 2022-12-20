d877b69c1dd9f692ca4c9dc7295e617a3e940cbf
Olami <8sinss@gmail.com>
2022-12-20 08:30:27 -0800

    task1-updated

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,16 +1,28 @@
 #!/usr/bin/python3
-# 1-square.py
-
-"""Define a class Square."""
 
 
 class Square:
-    """Represent a square."""
+    """Square class."""
 
     def __init__(self, size):
-        """Initialize a new Square.
+        """__init__ method for Square.
         Args:
-            size (int): The size of the new square.
+            size (int): size of Square.
         """
         self.__size = size
 
+
+if __name__ == '__main__':
+    my_square = Square(3)
+    print(type(my_square))
+    print(my_square.__dict__)
+
+    try:
+        print(my_square.size)
+    except Exception as e:
+        print(e)
+
+    try:
+        print(my_square.__size)
+    except Exception as e:
+        print(e)
