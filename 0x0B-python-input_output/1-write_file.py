6278985d7d1295f590d5232f00c7a7da5450b396
Olami <8sinss@gmail.com>
2023-01-10 09:02:49 -0800

    task1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,21 +1,12 @@
 #!/usr/bin/python3
+# 1-number_of_lines.py
+""Defines a text file line-counting function."""
 
 
 def number_of_lines(filename=""):
-    """Count number of lines in file
-    Args:
-        filename (str): string of path to file
-    Returns:
-        number of lines in file
-    """
-    nb_lines = 0
-    with open(filename, 'r', encoding='utf-8') as f:
+    """Return the number of lines in a text file."""
+    lines = 0
+    with open(filename) as f:
         for line in f:
-            nb_lines += 1
-    return nb_lines
-
-
-if __name__ == '__main__':
-    filename = "my_file_0.txt"
-    nb_lines = number_of_lines(filename)
-    print("{} has {:d} lines".format(filename, nb_lines))
+            lines += 1
+    return 
