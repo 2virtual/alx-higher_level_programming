83c04d23409a912b86f7132c2a0b4731010be0a0
Olami <8sinss@gmail.com>
2023-03-20 03:07:06 -0700

    updated 7,5 and 14

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,35 +1,19 @@
-
 #!/usr/bin/python3
-"""Script lists all cities from a given state
-   and protects against SQL injections
-   Script should take 4 arguments:
-   mysql username, mysql-password, database-name, state-name
-"""
+# Displays all cities of a given state from the
+# states table of the database hbtn_0e_4_usa.
+# Safe from SQL injections.
+# Usage: ./5-filter_cities.py <mysql username> \
+#                             <mysql password> \
+#                             <database name> \
+#                             <state name searched>
+import sys
+import MySQLdb
 
-if __name__ == '__main__':
-    import sys
-    import MySQLdb
-
-    conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        charset="utf8"
-    )
-    cur = conn.cursor()
-    query = """SELECT name FROM cities
-    WHERE state_id = (SELECT id FROM states WHERE name = %s)
-    ORDER BY id ASC"""
-    cur.execute(query, (sys.argv[4],))
-    query_rows = cur.fetchall()
-
-    query_list = []
-    for row in query_rows:
-        query_list.append(row[0])
-    state_str = ", ".join(query_list)
-    print(state_str)
-
-    cur.close()
-    conn.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `cities` as `c` \
+                INNER JOIN `states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` \
+                ORDER BY `c`.`id`")
+    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
