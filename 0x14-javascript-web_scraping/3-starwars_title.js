ce57d355f7c03765698b3f85786298990d4ecdb4
Olami <8sinss@gmail.com>
2023-04-25 22:14:20 -0700

    task3 update

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,19 +1,17 @@
 #!/usr/bin/node
-
 const request = require('request');
+const movieID = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
 
-const options = {
-  url: 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2],
-  headers: {
-    'User-Agent': 'request'
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const movie = JSON.parse(body);
+    if (movie.episode_id == movieID) {
+      console.log(movie.title);
+    } else {
+      console.log(`No Star Wars movie found with episode number ${movieID}`);
+    }
   }
-};
-
-function callback (error, response, body) {
-  if (!error && response.statusCode === 200) {
-    const info = JSON.parse(body);
-    console.log(info['title']);
-  }
-}
-
-request(options, callback);
+});
